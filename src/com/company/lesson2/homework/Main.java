c05d76306fdffc4b8d69f107851d68fe6058b8d6
Nazar <stanchenkon@gmail.com>
2021-12-22 01:18:30 +0200

    inital commit

---
+++ src/com/company/lesson2/homework/Main.java
@@ -0,0 +1,8 @@
+package com.company.lesson2.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет");
+        System.out.println("Привет");
+    }
+}
---
+++ src/com/company/lesson2/listing1/Main.java
@@ -0,0 +1,12 @@
+package com.company.lesson2.listing1;
+
+//import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("Second try");
+        System.out.print("Hello\n\n\nWorld!");
+    }
+}
