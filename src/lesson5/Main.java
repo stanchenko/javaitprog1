de29eb3a8e7a4545b005e811b305633c9365ce74
Nazar Stanchenko <nazarstanchenko@MacBook-Pro-Nazar.local>
2023-03-05 16:32:42 +0200

    finish lesson5

---
+++ src/lesson5/Main.java
@@ -0,0 +1,10 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        System.out.println("Your string is: " + str.nextLine());
+    }
+}
