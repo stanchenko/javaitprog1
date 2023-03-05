62f57f65d89b748fd4541d0c13ee591ac216b39b
Nazar Stanchenko <nazarstanchenko@MacBook-Pro-Nazar.local>
2023-03-05 12:55:21 +0200

    finish lesson4

--- src/com/company/lesson2/homework/Main.java
+++ src/com/company/lesson2/homework/Main.java
@@ -2,7 +2,8 @@ package com.company.lesson2.homework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет");
-        System.out.println("Привет");
+        System.out.println("Hello World!");
+        System.out.print("Hello World!");
+
     }
 }
---
+++ src/lesson4/Main.java
@@ -0,0 +1,29 @@
+package lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        byte num; //створили змінну
+        num = 1; //призначили значення
+        byte num2 = 23; // створили та відразу призначили
+        //byte від -127 до 127
+        System.out.println(num);
+        System.out.println(num2);
+
+        int num3 = 34656748;
+        System.out.println(num3);
+
+        float num4 = 23.45f;
+        double num5 = 234.345667d;
+        System.out.println(num4);
+        System.out.println(num5);
+
+        boolean isTrue = true; // true =1 false=0;
+        System.out.println(isTrue);
+
+        char symvol = 'R';
+        System.out.println(symvol);
+
+        String str = "Hello World";
+        System.out.println(str);
+    }
+}
