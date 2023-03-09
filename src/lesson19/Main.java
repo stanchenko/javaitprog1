976b04ebc6635f6c8044b69bc615dd337da7dc44
Nazar <stanchenkon@gmail.com>
2023-03-09 23:19:33 +0200

    finish lesson19 пакети та області

---
+++ src/lesson19/Main.java
@@ -0,0 +1,13 @@
+package lesson19;
+
+public class Main {
+
+  // int x = 0; змінна написана вне метода не буде відна в методі
+
+    public static void main(String[] args) {
+       int x = 0;
+        System.out.println(x); // щоб була доступна зовнішня змінна, треда використовувати this.x
+    }
+
+
+}
